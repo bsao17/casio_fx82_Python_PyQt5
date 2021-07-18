128a21efa268bddbc65d9c86c4017d67ee4049c1
bsao17 <declic62@gmail.com>
2021-07-18 21:13:28 +0200

    init casio_fs-82

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ from ui_casio_template import *
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Ui_MainWindow()
-    window.state = "-X-XO----"
-    window.show()
-    sys.exit(app.exec())
