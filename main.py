d530f4f0289ea948f9ce76dcdd4d3deee74fe072
bsao17 <declic62@gmail.com>
2021-07-19 18:46:35 +0200

    Add class and templates props

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+"""import sys
+from PySide2 import QtGui
+from PySide2.QtWidgets import QDialog, QApplication
+
+class MyWindow(QDialog, QtGui):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+"""
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
-import ui_casio_template as Casio
+from PySide2.QtWidgets import QDialog, QApplication
 
 
 class MyWindow(QDialog):
-    def __init__(self):
+    def __init__(self, width, height):
         super(MyWindow, self).__init__()
+        self.width = width
+        self.height = height
+        self.setWindowTitle('Casio Fx-82')
+        self.resize(self.width, self.height)
 
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    form = MyWindow()
-    form.show()
-    app.exec_()
+app = QApplication(sys.argv)
+casio = MyWindow(660, 1000)
+casio.show()
+sys.exit(app.exec_())
