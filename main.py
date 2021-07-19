c5e9d3bbc2af13ee612f80c9df512963b5072f56
bsao17 <declic62@gmail.com>
2021-07-19 19:10:04 +0200

    Import template casio okay

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-"""import sys
-from PySide2 import QtGui
-from PySide2.QtWidgets import QDialog, QApplication
+import sys
 
-class MyWindow(QDialog, QtGui):
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import QDialog, QApplication
+from casio_template import Ui_MainWindow
+
+
+class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-"""
-import sys
-from PySide2.QtWidgets import QDialog, QApplication
-
-
-class MyWindow(QDialog):
-    def __init__(self, width, height):
-        super(MyWindow, self).__init__()
-        self.width = width
-        self.height = height
+        self.setupUi(self)
         self.setWindowTitle('Casio Fx-82')
-        self.resize(self.width, self.height)
 
 
 app = QApplication(sys.argv)
-casio = MyWindow(660, 1000)
+casio = MyWindow()
 casio.show()
 sys.exit(app.exec_())
