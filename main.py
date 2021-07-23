b41fa4b4301d8c9c99a29e2a17e3aadd394c042d
bsao17 <declic62@gmail.com>
2021-07-23 08:59:25 +0200

    Connect button && add function display

--- main.py
+++ main.py
@@ -10,52 +10,100 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MyWindow, self).__init__()
         self.setupUi(self)
         self.setWindowTitle('Casio Fx-82')
-        # self.b1.clicked.connect(self.addText())
-        
+        self.numbers = ""
+        self.numberTwo = ""
+        self.operator = ""
+        self.total = []
+
+
+
+# Display number
     def displayZero(self):
         self.lineEdit.setText(self.lineEdit.text() + "0")
+
 
     def displayOne(self):
         self.lineEdit.setText(self.lineEdit.text() + "1")
 
+
     def displayTwo(self):
         self.lineEdit.setText(self.lineEdit.text() + "2")
+
 
     def displayThree(self):
         self.lineEdit.setText(self.lineEdit.text() + "3")
 
+
     def displayFour(self):
         self.lineEdit.setText(self.lineEdit.text() + "4")
+
 
     def displayFive(self):
         self.lineEdit.setText(self.lineEdit.text() + "5")
 
+
     def displaySix(self):
         self.lineEdit.setText(self.lineEdit.text() + "6")
+
 
     def displaySeven(self):
         self.lineEdit.setText(self.lineEdit.text() + "7")
 
+
     def displayEight(self):
         self.lineEdit.setText(self.lineEdit.text() + "8")
+
 
     def displayNine(self):
         self.lineEdit.setText(self.lineEdit.text() + "9")
 
-    def displayMore(self):
-        self.lineEdit.setText(self.lineEdit.text() + "+")
 
-    def displayLess(self):
-        self.lineEdit.setText(self.lineEdit.text() + "-")
+# Operators
+    def bmore(self):
+        self.numbers = (int(self.lineEdit.text()))
+        self.operator = "+"
+        self.lineEdit.clear()
+        print(self.numbers)
 
-    def displayNoun(self):
-        self.lineEdit.setText(self.lineEdit.text() + "*")
+    def bless(self):
+        self.numbers = (int(self.lineEdit.text()))
+        self.operator = "-"
+        self.lineEdit.clear()
+        print(self.numbers)
 
-    def displayDivide(self):
-        self.lineEdit.setText(self.lineEdit.text() + "/")
+    def bnoun(self):
+        self.numbers = (int(self.lineEdit.text()))
+        self.operator = "*"
+        self.lineEdit.clear()
+        print(self.numbers)
+
+    def bdivide(self):
+        self.numbers = (int(self.lineEdit.text()))
+        self.operator = "/"
+        self.lineEdit.clear()
+        print(self.numbers)
 
     def erase(self):
-        self.lineEdit.setText(self.lineEdit.text() + "C")
+        self.lineEdit.clear()
+
+    def pressEqual(self):
+        self.numberTwo = (int(self.lineEdit.text()))
+        if self.operator == "+":
+            total = int(self.numbers) + int(self.numberTwo)
+            print(total)
+            self.lineEdit.setText(str(total))
+        elif self.operator == "-":
+            total = int(self.numbers) - int(self.numberTwo)
+            self.lineEdit.setText(str(total))
+        elif self.operator == "*":
+            total = int(self.numbers) * int(self.numberTwo)
+            self.lineEdit.setText(str(total))
+        elif self.operator == "/":
+            total = int(self.numbers) / int(self.numberTwo)
+            self.lineEdit.setText(str(total))
+
+
+
 
 
 if __name__ == '__main__':
@@ -72,4 +120,10 @@ if __name__ == '__main__':
     casio.b8.clicked.connect(casio.displayEight)
     casio.b9.clicked.connect(casio.displayNine)
     casio.b0.clicked.connect(casio.displayZero)
+    casio.bc.clicked.connect(casio.erase)
+    casio.more.clicked.connect(casio.bmore)
+    casio.less.clicked.connect(casio.bless)
+    casio.noun.clicked.connect(casio.bnoun)
+    casio.divide.clicked.connect(casio.bdivide)
+    casio.equal.clicked.connect(casio.pressEqual)
     sys.exit(app.exec_())
